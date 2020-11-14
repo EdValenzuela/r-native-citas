14303dc31ff3bd0eef7ef498bcdcbc40a81eafde
Eduardo Valenzuela <e.valenzuelap@alumnos.duoc.cl>
2020-11-14 03:22:17 -0300

    🎉 Commit Initial

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+  Text
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  return (
+    <>
+      <Text>Proyecto Citas</Text>
+    </>
+  );
+};
+// 
+export default App;
